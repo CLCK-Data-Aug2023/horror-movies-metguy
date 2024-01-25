55bd0aadbd7a71eb70da2841b8d3802fcee16af0
Emmett Blair <emmettblair.eb@gmail.com>
2024-01-25 00:04:31 -0500

    added missing keyword and parameters to query

--- sql/horror_movies.sql
+++ sql/horror_movies.sql
@@ -1,6 +1,6 @@
-SELECT id, name, imdb_rating
+SELECT id AS 'Movie_ID', name AS 'Movie_Title', imdb_rating AS 'Rating'
 FROM movies
-WHERE year < 1985
-OR year = 1985
+WHERE year < 1986
+AND genre = 'horror'  
 ORDER BY imdb_rating DESC
 LIMIT 3;
